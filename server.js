3b70706f24e249a72499cabebac909d443370ee8
alejandro <AlejP2121>
2021-09-27 12:55:50 +0200

    fix

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: "false" }));
 app.use(express.json());
+app.use(express.raw());
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
